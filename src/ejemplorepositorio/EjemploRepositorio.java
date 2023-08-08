c32bf1a148e0095208234bd4f7632ff1d488bcd9
lucianaamet <luciana_amet@hotmail.com>
2023-08-08 17:05:15 -0300

    desde visual

--- src/ejemplorepositorio/EjemploRepositorio.java
+++ src/ejemplorepositorio/EjemploRepositorio.java
@@ -7,6 +7,7 @@ public class EjemploRepositorio {
    
     public static void main(String[] args) {
         System.out.println("modificacion!!");
-    }
+        System.out.println("desde vsc");    
+       }
     
 }
